e5460202e8090c7d3f05ff581c42f65b54f34e81
mattsegovia <segoviamatt@hotmail.com>
2020-09-06 20:55:57 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Matthew Segovia on 9/6/20.
+//  Copyright © 2020 Matthew Segovia. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
